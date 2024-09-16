65397cc25a055027e90eb055971ff1c741cd01ae
Kamal Chaudhari <42417060+kchaudhari0@users.noreply.github.com>
2024-09-16 23:09:05 +0530

    Time: 1 ms (99.78%), Space: 52.6 MB (68.27%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
         
-        HashMap<Integer , Integer> hm= new HashMap();
-        int count=0;
-        int ele=0;
+        //working in solving this in O(1) space , we will be using moore voting alogorithm
         
-        for(int n:nums)
-        {
-            hm.put(n,1+hm.getOrDefault(n,0));
-            if(hm.get(n)>count)
-            {
-                ele=n;
-                count=hm.get(n);
+        int candidate=0;
+        int count =0;
+        
+         for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
             }
+            count += (num == candidate) ? 1 : -1;
         }
         
-        return ele;
+        return candidate;
+        
     }
 }
